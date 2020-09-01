327283ab4c0c7e5f67a1eb68f534767dba05edae
Max Duval <max@mxdvl.com>
2020-09-01 14:21:33 +0100

    Create an Image component
    - move image files from `assets` to `images`
    - update the Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'assets',
-        path: `${__dirname}/src/content/assets`
+        name: 'images',
+        path: `${__dirname}/src/content/images`
       }
     },
     {
---
+++ src/components/Image.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from '@emotion/styled'
+
+interface ImageProps {
+  filename: string
+  alt?: string
+}
+
+const StyledImg = styled(Img)`
+  width: 80%;
+`
+
+export const Image: React.FC<ImageProps> = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        images: allFile(filter: { relativeDirectory: { glob: "images" } }) {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                sizes(maxWidth: 600) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const image = data.images.edges.find((n: Record<string, any>) => {
+        return n.node.relativePath.includes(props.filename)
+      })
+      if (!image) {
+        return <div>No image found :(</div>
+      }
+
+      const imageSizes = image.node.childImageSharp.sizes
+      return <StyledImg alt={props.alt} sizes={imageSizes} />
+    }}
+  />
+)
