9625dec6049e28e4a633c584872b7fa0581648eb
Josh Buckland <buck06191@gmail.com>
2020-10-26 11:28:26 +0000

    Move the homepage over to MDX
    We want to store content inside MDX files rather than `.tsx` files. This
    moves the existing index page over to MDX to keep in line with this.

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -4,17 +4,24 @@ import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { MDXProvider } from '@mdx-js/react'
 import Page from '../components/Page'
-import Container from '../components/Container'
+import { BodyContainer } from '../components/HomePage/BodyContainer'
 import ContentSection from '../components/ContentSection'
 import Content from '../components/Content'
 import { ContentLeft, ContentRight } from '../components/ContentBox'
+import { Headline, CallToAction } from '../components/HomePage/Headline'
+import { Image } from '../components/Image'
+import { LearnMoreButton } from '../components/HomePage/LearnMoreButton'
 import IndexLayout from '../layouts'
 
 const shortcodes = {
   ContentSection,
   Content,
   ContentLeft,
-  ContentRight
+  ContentRight,
+  Headline,
+  CallToAction,
+  Image,
+  LearnMoreButton
 }
 interface PageTemplateProps {
   data: {
@@ -40,12 +47,12 @@ const PageTemplate: React.FC<PageTemplateProps> = ({ data }) => {
   return (
     <IndexLayout>
       <Page>
-        <Container>
+        <BodyContainer>
           <h1>{post.frontmatter.title}</h1>
           <MDXProvider components={shortcodes}>
             <MDXRenderer>{post.body}</MDXRenderer>
           </MDXProvider>
-        </Container>
+        </BodyContainer>
       </Page>
     </IndexLayout>
   )
