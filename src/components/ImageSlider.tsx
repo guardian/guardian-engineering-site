a4b3e87e9e68181a3daa317b3164511a331cb0f0
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-11-26 12:37:25 +0200

    add ImageSlider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import BackgroundSlider from 'gatsby-image-background-slider'
-import { useStaticQuery } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 interface ImageSliderProps {
   className?: string
@@ -8,23 +8,35 @@ interface ImageSliderProps {
   imgAlt?: string
 }
 
+const images = ['images/Ellipse 2.1.png', 'images/home-about-gnm.png', 'images/home-about-gnm.jpg']
+
 const ImageSlider: React.FC<ImageSliderProps> = () => (
-  <BackgroundSlider
-    query={useStaticQuery(graphql`
-      query {
-        backgrounds: allFile(filter: { sourceInstanceName: { eq: "images" } }) {
-          nodes {
-            relativePath
-            childImageSharp {
-              fluid(maxWidth: 4000, quality: 100) {
-                ...GatsbyImageSharpFluid
+  <div>
+    <BackgroundSlider
+      images={images}
+      query={useStaticQuery(graphql`
+        query {
+          backgrounds: allFile(filter: { relativeDirectory: { glob: "images" } }) {
+            nodes {
+              relativePath
+              name
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
+                }
               }
             }
           }
         }
-      }
-    `)}
-  />
+      `)}
+      initDelay={2} // delay before the first transition (if left at 0, the first image will be skipped initially)
+      transition={4} // transition duration between images
+      duration={8} // how long an image is shown
+      style={{
+        transform: 'rotate(-2deg) scale(.9)'
+      }}
+    />
+  </div>
 )
 
 export default ImageSlider
