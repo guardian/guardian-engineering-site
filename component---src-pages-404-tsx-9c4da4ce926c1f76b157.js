e694eaaabcb579747f460481879868ff8e107349
buck06191 <buck06191@users.noreply.github.com>
2021-03-18 17:18:45 +0000

    deployed via Gatsby Publish Action 🎩 for 95d97f4632e78bff2101c5b3c7ca303b3117a3a3

---
+++ component---src-pages-404-tsx-9c4da4ce926c1f76b157.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(t,n,e){"use strict";e.r(n);e("q1tI");var o=e("Wbzz"),u=e("NqE+"),c=e("TBFr"),d=e("+ego"),l=e("qKvR");n.default=function(){return Object(l.d)(d.a,null,Object(l.d)(u.a,null,Object(l.d)(c.a,null,Object(l.d)("h1",null,"404: Page not found."),Object(l.d)("p",null,"You've hit the void. ",Object(l.d)(o.a,{to:"/"},"Go back.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-9c4da4ce926c1f76b157.js.map
