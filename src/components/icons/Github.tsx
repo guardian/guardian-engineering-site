e350f0ab632de05c4022e35227188a0a7d4605e4
Max Duval <max.duval@theguardian.com>
2022-02-28 09:29:59 +0000

    fix: use astro styles for social icons
    Emotion isn’t working properly, so let’s keep styles native to Astro.
    Styles are scoped by default, so Emotion is not adding much.
    
    Using Source is not a priority, or much of an aim. As long as
    this implementation is as accessible as Source’s, we are fine.
    
    We can remove unused icons component, and only use the icons as SVG.

--- src/components/icons/Github.tsx
+++ src/components/icons/Github.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-import { LinkButton } from "@guardian/source-react-components";
-import { buttonOverrides } from "../../styles/shared";
-
-const SvgGithub = () => {
+export const SvgGithub = () => {
   return (
     <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
@@ -13,15 +9,3 @@ const SvgGithub = () => {
     </svg>
   );
 };
-
-export const LinkGithub = () => (
-  <LinkButton
-    cssOverrides={buttonOverrides}
-    href="https://github.com/guardian"
-    target="_blank"
-    hideLabel={true}
-    icon={<SvgGithub />}
-  >
-    Github @guardian
-  </LinkButton>
-);
