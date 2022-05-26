d8fd8bb64dabbdb7cafe4c8048c5706faef59365
Max Duval <max.duval@theguardian.com>
2022-05-26 18:23:30 +0100

    feat(astro): get to v1 (beta)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  renderers: ["@astrojs/renderer-react", "@astrojs/renderer-svelte"],
-  buildOptions: {
-    site: "https://theguardian.engineering/",
-  },
+export default defineConfig({
+  integrations: [react(), svelte()],
+  site: "https://theguardian.engineering/",
 });
